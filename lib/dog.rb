48e871b657b7cf4146b1801cc7ee11095e0e8f97
Erin Mettille <ecmettille@gmail.com>
2020-04-21 18:22:25 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name 
+  end
+  
+  def name 
+    @this_dogs_name 
+  end
+  
+  def breed=(dog_breed)
+    @this_dog_name = dog_breed
+  end
+  
+  def breed 
+    @this_dog_name
+  end
+  
+end
